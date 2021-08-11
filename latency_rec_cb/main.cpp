b5c1b14367ae5fb2915b93fcf0e3e21eedf85091
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-08-11 19:26:54 +0200

    first running version

--- latency_rec_cb/main.cpp
+++ latency_rec_cb/main.cpp
@@ -1,30 +1,34 @@
-#include <memory>
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-class StringSubscriber : public rclcpp::Node
+class LatencyRec : public rclcpp::Node
 {
 public:
-  StringSubscriber()
-  : Node("string_subscriber")
+  LatencyRec()
+  : Node("LatencyRec")
   {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "String",
+    sub_ = this->create_subscription<std_msgs::msg::String>(
+      "pkg_send",
       10,
       [this](std_msgs::msg::String::UniquePtr msg) {
-        RCLCPP_INFO(this->get_logger(), "Received a message");
+        this->pub_->publish(*msg);
+        //RCLCPP_INFO(this->get_logger(), "Received : %i bytes", msg->data.size());
       });
+
+    pub_ = this->create_publisher<std_msgs::msg::String>(
+      "pkg_reply",
+      10);
   }
 
 private:
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr    pub_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StringSubscriber>());
+  rclcpp::spin(std::make_shared<LatencyRec>());
   rclcpp::shutdown();
   return 0;
 }
--- latency_snd/main.cpp
+++ latency_snd/main.cpp
@@ -3,36 +3,106 @@
 
 using namespace std::chrono_literals;
 
-class StringPublisher : public rclcpp::Node
+class LatencySnd : public rclcpp::Node
 {
 public:
-  StringPublisher()
-  : Node("string_publisher")
+  LatencySnd()
+    : Node("LatencySnd")
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("String", 10);
-    message_ = std_msgs::msg::String();
-    message_.data = "Hello, world! ";
-    for(auto i = 0; i < 14; ++i) message_.data += message_.data;
-    
-      auto timer_callback =
+    // parameter
+    snd_size_ = 1024; // kB
+    runs_     = 200;
+
+    // log test
+    std::cout << "-----------------------------------------------------------------" << std::endl;
+    std::cout << " LATENCY / THROUGHPUT TEST"                                        << std::endl;
+    std::cout << " RUNS  : " << runs_                                                << std::endl;
+    std::cout << " SIZE  : " << snd_size_ << " kB"                                   << std::endl;
+    std::cout << "-----------------------------------------------------------------" << std::endl;
+
+    // create payload
+    msg_ = std_msgs::msg::String();
+    msg_.data = "Hello, world! ";
+    snd_size_ *= 1024;
+    while (msg_.data.size() < snd_size_) msg_.data += msg_.data;
+    msg_.data.resize(snd_size_);
+
+    // prepare timestamp array
+    diff_array_.reserve(runs_);
+    snd_time_ = 0;
+    snd_pkgs_ = 0;
+
+    // create publisher
+    pub_ = this->create_publisher<std_msgs::msg::String>(
+      "pkg_send",
+      10);
+
+    // create subscriber
+    sub_ = this->create_subscription<std_msgs::msg::String>(
+      "pkg_reply",
+      10,
+      [this](std_msgs::msg::String::UniquePtr msg) {
+        this->diff_array_.push_back(GetMicroSeconds() - this->snd_time_);
+        //RCLCPP_INFO(this->get_logger(), "Received : %i bytes\n", msg->data.size());
+      });
+
+    auto timer_callback =
       [this]() -> void {
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message_.data.c_str());
-        this->publisher_->publish(this->message_);
-      };
-      
+      // store send time
+      this->snd_time_ = GetMicroSeconds();
+      // send message
+      this->pub_->publish(this->msg_);
+      //RCLCPP_INFO(this->get_logger(), "Sent     : %i bytes", msg_.data.size());
+
+      // check for termination
+      if (this->snd_pkgs_ >= this->runs_)
+      {
+        // stop timer
+        timer_->cancel();
+        // destroy subscriber
+        sub_ = nullptr;
+
+        // calculate roundtrip time over all received messages
+        long long sum_time = std::accumulate(this->diff_array_.begin(), this->diff_array_.end(), 0LL);
+        long long avg_time = sum_time / this->diff_array_.size();
+        std::cout << std::endl;
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+        std::cout << "Messages sent                      : " << this->snd_pkgs_          << std::endl;
+        std::cout << "Messages received                  : " << this->diff_array_.size() << std::endl;
+        std::cout << "Sum time                           : " << sum_time/1000 << " ms"   << std::endl;
+        std::cout << "Message average roundtrip time     : " << avg_time   << " us"      << std::endl;
+        std::cout << "Message average roundtrip time / 2 : " << avg_time/2 << " us"      << std::endl;
+        std::cout << "-----------------------------------------------------------------" << std::endl;
+      }
+      this->snd_pkgs_++;
+    };
     timer_ = this->create_wall_timer(100ms, timer_callback);
   }
 
 private:
-  rclcpp::TimerBase::SharedPtr                         timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr  publisher_;
-  std_msgs::msg::String                                message_;
+  long long GetMicroSeconds()
+  {
+    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+    return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
+  }
+
+  rclcpp::TimerBase::SharedPtr                           timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr    pub_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
+  std_msgs::msg::String                                  msg_;
+
+  int                                                    runs_;
+  int                                                    snd_size_;
+
+  std::vector<long long>                                 diff_array_;
+  long long                                              snd_time_;
+  int                                                    snd_pkgs_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<StringPublisher>());
+  rclcpp::spin(std::make_shared<LatencySnd>());
   rclcpp::shutdown();
   return 0;
 }
