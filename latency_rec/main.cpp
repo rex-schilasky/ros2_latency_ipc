7d0386f888b2f31a4b0597724d86a588b947ad68
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2021-08-14 17:22:55 +0200

    latency_rec keeps running simplified single shot solution

--- latency_rec/main.cpp
+++ latency_rec/main.cpp
@@ -5,8 +5,7 @@ class LatencyRec : public rclcpp::Node
 {
 public:
   LatencyRec(bool log_it)
-    : Node("LatencyRec"),
-    log_it_(log_it)
+    : Node("LatencyRec"), log_it_(log_it)
   {
     // prepare timestamp array to avoid allocations
     latency_array_.reserve(10000);
@@ -14,10 +13,7 @@ public:
     // define qos
     auto qos = rclcpp::QoS(rclcpp::KeepLast(0)).best_effort().durability_volatile();
 
-    // create publisher for topic 'pong'
-    pub_ = this->create_publisher<std_msgs::msg::String>("pong", qos);
-
-    // create subscriber for topic 'pkg_snd'
+    // create subscriber for topic 'ping'
     sub_ = create_subscription<std_msgs::msg::String>("ping", qos, std::bind(&LatencyRec::OnReceive, this, std::placeholders::_1));
   }
 
@@ -32,33 +28,26 @@ public:
     // final message ? :-)
     if (snd_time == 42)
     {
-      std::cout << "----------------------------------------"                 << std::endl;
-      std::cout << "Messages received       : " << latency_array_.size()      << std::endl;
+      std::cout << "----------------------------------------"                << std::endl;
+      std::cout << "Messages received       : " << latency_array_.size()     << std::endl;
       if (!latency_array_.empty())
       {
         long long sum_time = std::accumulate(latency_array_.begin(), latency_array_.end(), 0LL);
         long long avg_time = sum_time / latency_array_.size();
-        std::cout << "Message size            : " << rec_size_/1024 << " kB"  << std::endl;
-        std::cout << "Message average latency : " << avg_time << " us"        << std::endl;
+        std::cout << "Message size            : " << rec_size_/1024 << " kB" << std::endl;
+        std::cout << "Message average latency : " << avg_time << " us"       << std::endl;
       }
-      std::cout << "----------------------------------------"                 << std::endl;
+      std::cout << "----------------------------------------"                << std::endl;
 
-      // shutdown here
-      rclcpp::shutdown();
+      // reset latency array and receive size
+      latency_array_.clear();
+      rec_size_ = 0;
     }
     else
     {
-      // calculate latency
+      // calculate latency and store latency
       auto latency = rec_time - snd_time;
-
-      // store latency for later experiment evaluation
       latency_array_.push_back(latency);
-
-      // store new send time into msg
-      *reinterpret_cast<long long*>(&msg->data[0]) = get_microseconds();
-
-      // and publish back
-      pub_->publish(*msg);
 
       // store receive time
       rec_size_ = msg->data.size();
@@ -76,13 +65,10 @@ private:
     return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
   }
 
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr    pub_;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
-
   std::vector<long long>                                 latency_array_;
-
   size_t                                                 rec_size_ = 0;
-  bool                                                   log_it_ = false;
+  bool                                                   log_it_   = false;
 };
 
 int main(int argc, char* argv[])
--- latency_snd/main.cpp
+++ latency_snd/main.cpp
@@ -21,20 +21,11 @@ public:
     snd_size_ *= 1024;
     msg_.data.resize(snd_size_);
 
-    // prepare timestamp array to avoid allocations
-    latency_array_.reserve(runs_);
-
     // define qos
     auto qos = rclcpp::QoS(rclcpp::KeepLast(0)).best_effort().durability_volatile();
 
     // create publisher for topic 'ping'
     pub_ = create_publisher<std_msgs::msg::String>("ping", qos);
-
-    // create subscriber for topic 'pong'
-    sub_ = create_subscription<std_msgs::msg::String>("pong", qos, std::bind(&LatencySnd::OnReceive, this, std::placeholders::_1));
-
-    // make a short sleep to get all matched, maybe an underlying RMW/DDS needs this
-    rclcpp::sleep_for(500ms);
 
     // finally create and start timer for publishing
     auto timer_delay = std::chrono::milliseconds(delay);
@@ -46,7 +37,7 @@ public:
     // check for termination
     if (snd_pkgs_ < runs_)
     {
-      // store send time
+      // store send time into string (bad style for sure)
       *reinterpret_cast<long long*>(&msg_.data[0]) = get_microseconds();
 
       // and publish the message
@@ -61,46 +52,16 @@ public:
       // stop timer
       timer_->cancel();
 
-      // destroy subscriber
-      sub_ = nullptr;
-
-      // send a bunch of final messages
-      // sometimes we loose ones by BEST_EFFORT :-/
+      // send final messages to trigger latency_rec to summarize
       *reinterpret_cast<long long*>(&msg_.data[0]) = 42;
-      for(auto frun = 0; frun < 10; ++frun)
-      {
-        pub_->publish(msg_);
-        rclcpp::sleep_for(100ms);
-      }
+      pub_->publish(msg_);
 
-      std::cout << "Messages sent           : " << snd_pkgs_               << std::endl;
-      std::cout << "Messages received       : " << latency_array_.size()   << std::endl;
-
-      // calculate roundtrip time over all received messages
-      if (!latency_array_.empty())
-      {
-        long long sum_time = std::accumulate(latency_array_.begin(), latency_array_.end(), 0LL);
-        long long avg_time = sum_time / latency_array_.size();
-        std::cout << "Message average latency : " << avg_time << " us"     << std::endl;
-      }
-      std::cout << "----------------------------------------"              << std::endl;
+      std::cout << "Messages sent           : " << snd_pkgs_  << std::endl;
+      std::cout << "----------------------------------------" << std::endl;
 
       // shutdown here
       rclcpp::shutdown();
     }
-  }
-
-  void OnReceive(std_msgs::msg::String::UniquePtr msg)
-  {
-    // calculate latency
-    auto latency = get_microseconds() - *reinterpret_cast<long long*>(&msg->data[0]);
-
-    // store latency for later experiment evaluation
-    latency_array_.push_back(latency);
-    
-    // log it
-    if (log_it_) RCLCPP_INFO(get_logger(), "Received : %li bytes", msg->data.size());
-    if (log_it_) RCLCPP_INFO(get_logger(), "Latency  : %lli us\n", latency);
   }
 
 private:
@@ -110,49 +71,35 @@ private:
     return(std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count());
   }
 
-  rclcpp::TimerBase::SharedPtr                           timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr    pub_;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
-  std_msgs::msg::String                                  msg_;
-
-  std::vector<long long>                                 latency_array_;
-  size_t                                                 snd_pkgs_ = 0;
-
-  size_t                                                 runs_     = 0;
-  size_t                                                 snd_size_ = 0;
-  size_t                                                 delay_    = 0;
-  bool                                                   log_it_   = false;
+  rclcpp::TimerBase::SharedPtr                        timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+  std_msgs::msg::String                               msg_;
+  size_t                                              snd_pkgs_ = 0;
+  size_t                                              runs_     = 0;
+  size_t                                              snd_size_ = 0;
+  size_t                                              delay_    = 0;
+  bool                                                log_it_   = false;
 };
 
 int main(int argc, char* argv[])
 {
-  // initialize
   rclcpp::init(argc, argv);
-
-  // paramter 'runs'
-  int runs(100);
+  int runs(100); // number of publications
   {
     char* cli_option = rcutils_cli_get_option(argv, argv + argc, "-r");
     if (cli_option) runs = std::atoi(cli_option);
   }
-  // parameter 'size' in kB
-  int snd_size(1);
+  int snd_size(1); // message size in kB
   {
     char* cli_option = rcutils_cli_get_option(argv, argv + argc, "-s");
     if (cli_option) snd_size = std::atoi(cli_option);
   }
-  // parameter 'delay' in ms
-  int delay(100);
+  int delay(100); // delay between two publications in ms
   {
     char* cli_option = rcutils_cli_get_option(argv, argv + argc, "-d");
     if (cli_option) delay = std::atoi(cli_option);
   }
-
-  // spin it
   rclcpp::spin(std::make_shared<LatencySnd>(runs, snd_size, delay, false));
-
-  // shutdown
   rclcpp::shutdown();
-
   return 0;
 }
